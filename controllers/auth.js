e65f1cccb0f5e080e5661e0711daa251481f6a72
Aravind SJ <hanaravind5@gmail.com>
2022-04-14 21:14:25 +0530

    config changed

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,8 +3,9 @@ const bcrypt = require('bcrypt')
 const StreamChat = require('stream-chat').StreamChat
 const crypto = require('crypto')
 
-require('dotenv').config()
-
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').parse()
+}
 const api_key = process.env.STREAM_API_KEY
 const api_secret = process.env.STREAM_API_SECRET
 const app_id = process.env.STREAM_APP_ID
