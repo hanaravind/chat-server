e9fa80312be446022be22a0f4cbd8439eeb4558d
Aravind SJ <hanaravind5@gmail.com>
2022-04-14 20:38:54 +0530

    initial commit chat server

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const cors = require('cors')
+const authRoutes = require('./routes/auth')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+require('dotenv').config()
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const messageServiceSid = process.env.TWILIO_MESSAGING_SID
+const twilioClient = require('twilio')(accountSid, authToken)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('Hello World! ')
+})
+
+app.post('/', (req, res) => {
+    const { message, user:sender, members } = req.body
+    if(type === 'message.new') {
+        members
+        .filter((member) => member.user_id !== sender.id)
+        .forEach(({ user }) => {
+            if(!user.online) {
+                twilioClient.messages.create({
+                    body: `You have a new message from ${message.user.fulllName} - ${message.text}`,
+                    messagingServiceSid: messageServiceSid,
+                    to: user.phoneNumber
+                }).then(() => {
+                    console.log('Message Sentd')
+                }).catch((error) => {
+                    console.log(error)
+                })
+            }
+        })
+        return res.status(200).send('Message Sent')
+    }
+    return res.status(200).send('Not a new Message Request')
+})
+
+app.use('/auth', authRoutes)
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
